b0eadfbea0e7818e20f9638fdf39d17b2a8424b0
Brad Pickens <bradley_pickens@fws.gov>
2026-03-13 13:17:50 -0400

    Create Quantify image character.py

---
+++ Imagery mgt and preprocessing/Quantify image character.py
@@ -0,0 +1,57 @@
+import cv2
+import glob
+import os
+import numpy
+import skimage
+import pandas as pd
+
+from skimage.feature import graycomatrix, graycoprops
+
+# Input: files = directory of images, export_csv = csv that reports brightness, contrast for each image
+files = glob.glob("C:/BP/Annotation_analysis/cvat_images_good/*.jpg")
+
+export_csv = "C:/BP/Annotation_analysis/new_character_Mar10.csv"
+
+total_pixels = 6464 * 4848
+
+image_list = []
+contrast_sd_list = []
+saturation_percent_list = []
+contrast10_list = []
+
+x=0
+for file in files:
+    x= x+1
+    print(x)
+    file1 = os.path.basename(file)
+    print(file1)
+    img = cv2.imread(file)
+    image_list.append(file1)
+
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Contrast sd
+    contrast_sd =numpy.std(img_grey)
+    #print("Contrast sd: ", contrast_sd)
+    contrast_sd_list.append(contrast_sd)
+
+    # Calculate % saturation with global thresholding: INPUT THRESHOLD VALUE,
+    # If pixel value is greater than a threshold value, it is assigned one value
+    ret1, th1 = cv2.threshold(img_grey, 230, 1, cv2.THRESH_BINARY)
+    sum1 = numpy.sum(th1)
+
+    saturation_prop = sum1/ total_pixels
+    saturation_percent = saturation_prop*100
+    saturation_percent_list.append(saturation_percent)
+    print("saturation: ", saturation_percent)
+
+    # Texture analysis
+    glcm = graycomatrix(img_grey, distances=[10], angles=[45], levels=256, symmetric=True, normed=True)
+
+    # Normalizes data
+    contrast10 = graycoprops(glcm, 'contrast')
+    contrast10_list.append(contrast10)
+   # print("Local contrast: ", contrast10)
+
+pd.DataFrame({"unique_image": image_list, "contrast_sd": contrast_sd_list, "percent_saturation": saturation_percent_list,
+            "contrast": contrast10_list}).to_csv(export_csv, index=True)
