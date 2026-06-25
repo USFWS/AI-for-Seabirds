8c1d95dceaef90c504e70e1d7f25b757419b96e0
Brad Pickens <bradley_pickens@fws.gov>
2026-06-25 15:58:54 -0400

    config update

--- Imagery mgt and preprocessing/Copy move images from list.py
+++ Imagery mgt and preprocessing/Copy move images from list.py
@@ -1,31 +1,27 @@
 import pandas as pd
 import os
 import shutil
+import config
 
 # The script searches a image directory and copies/moves images that are listed in a csv file
 # the csv file should have column labeled as "unique_image_jpg"
 
 # Data inputs:
 # csv data= list of images to be copied/moved (column header of 'unique_image_jpg')
-# root_dir = directory of images to search
+# source_img = directory of images to search
 # dest1 = destination folder to move/copy images into
 
-#image_dir = "D:/SACR_models/SACR_FIX/survey_images_2025/"
-#csv_data = pd.read_csv("D:/SACR_models/SACR_FIX/yolov5x_2025_surveys_conf20_June9_gr5_cranes.csv")
-#dest1 = "D:/SACR_models/SACR_FIX/survey_images_2025_subset/"
+source_img = config.SOURCE_IMG
 
-# image_dir = "D:/WHCR_2025/1_parent_images/JPG_20250122_131500/"
-image_dir = "D:/seabird_winter/flights_2025/JPG_2025_Jan_Feb/Feb_22_23_2025/"
+csv_data = pd.read_csv(config.CSV_DATA)
 
-csv_data = pd.read_csv("D:/seabird_winter/winter_birds_missing.csv")
+export_dir = config.EXPORT_DIR
 
-dest1 = "D:/seabirds_Feb22_23/"
-
-if not os.path.exists(dest1):
-    os.mkdir(dest1)
+if not os.path.exists(export_dir):
+    os.mkdir(export_dir)
 
 ##if jpg is needed run this:
-csv_data['unique_image_jpg'] = csv_data['unique_image_jpg'] #+ ".jpg"
+csv_data['unique_image_jpg'] = csv_data['unique_image_jpg'] #+ ".png"
 
 x = 0
 
@@ -33,14 +29,13 @@ csv_list = []
 csv_list = csv_data['unique_image_jpg'].tolist()
 print(csv_list)
 
-for root, dirs, files in os.walk(image_dir):
+for root, dirs, files in os.walk(source_img):
     for filename in files:
         if filename in csv_list:
             x = x + 1
             print ("Copied: ", x)
             path = os.path.join(root, filename)
             print ("path " , path)
-            shutil.copy(path, dest1)  # can be shutil.move or shutil.copy
+            shutil.copy(path, export_dir)  # can be shutil.move or shutil.copy
         else:
             pass
-
